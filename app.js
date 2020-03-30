84a4883751e16b0d28664752296966c48b73f93d
mariolsun <mariolsun@yandex.ru>
2020-03-30 17:11:40 +0300

    некоторые правки

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(cookieParser());
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
+    console.log('crash-test error');
     throw new Error('Сервер сейчас упадет');
   }, 0);
 });
@@ -45,7 +46,7 @@ app.use(auth);
 app.use('/users', users);
 app.use('/cards', cards);
 app.use((req, res, next) => {
-  console.log(`404 error in app.js`);
+  console.log('404 error in app.js');
   next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
 
