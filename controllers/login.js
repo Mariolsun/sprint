36cc2e138e84943df98a848acc07ed5b582e1072
mariolsun <mariolsun@yandex.ru>
2020-04-05 00:21:12 +0300

    при авторизации теперь возвращается токен для локалстораджа

--- controllers/login.js
+++ controllers/login.js
@@ -21,7 +21,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: true,
       });
-      res.send({ message: 'Success!' });
+      res.send({ data: `Bearer ${token}` });
     })
     .catch(next);
 };
