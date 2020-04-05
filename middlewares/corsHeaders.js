7dcfc9a9937f952e3cf64396bc698daae9793894
mariolsun <mariolsun@yandex.ru>
2020-04-05 17:13:05 +0300

    cors fix

--- middlewares/corsHeaders.js
+++ middlewares/corsHeaders.js
@@ -10,6 +10,7 @@ module.exports = (req, res, next) => {
   // if (allowedCors.includes(origin)) {
   res.header('Access-Control-Allow-Origin', origin);
   // }
+  console.log(`addin cors headers`);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
