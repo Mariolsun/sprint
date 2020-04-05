3e1079ebd9b75b9ece8c09bcd7abd18a95c2a193
mariolsun <mariolsun@yandex.ru>
2020-04-05 15:38:28 +0300

    контроллер ошибок теперь возвращает сообщение ошибки в любом случае

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use((err, req, res, next) => {
   res
     .status(statusCode)
     .send({
-      message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+      message,
     });
 });
 
